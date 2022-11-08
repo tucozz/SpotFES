a74b2544872a9dff278872bec1341e59161d2fb8
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-11-08 11:13:39 -0300

    Adiciona Lista
    Co-authored-by: Tucozz <tucozz@users.noreply.github.com>

--- headers/lista.h
+++ headers/lista.h
@@ -1,12 +1,69 @@
+/**
+ * @file lista.h
+ * @brief Uma estrutura de dados de uma lista de tipo genérico
+ * 
+ *   Lista *lista_int = InicializaLista();
+ *
+ *   int *one1 = malloc(sizeof(int));
+ *   int *one2 = malloc(sizeof(int));
+ *   int *one3 = malloc(sizeof(int));
+ *   int *one4 = malloc(sizeof(int));
+ *   int *one5 = malloc(sizeof(int));
+ *   int *one6 = malloc(sizeof(int));
+ *
+ *   *one1 = 0;
+ *   *one2 = 1;
+ *   *one3 = 2;
+ *   *one4 = 3;
+ *   *one5 = 4;
+ *   *one6 = 5;
+ *
+ *   AdicionaElementoLista(lista_int, one1);
+ *   AdicionaElementoLista(lista_int, one2);
+ *   AdicionaElementoLista(lista_int, one3);
+ *   AdicionaElementoLista(lista_int, one4);
+ *   AdicionaElementoLista(lista_int, one5);
+ *   AdicionaElementoLista(lista_int, one6);
+ *
+ *   int *a = PopLista(lista_int);
+ *
+ *   printf("Integer (5): %d\n", *a);
+ *
+ *   free(a);
+ *
+ *   LiberaLista(lista_int, &free);
+ * 
+ * @version 0.1
+ * @date 2022-11-08
+ *  
+ */
+
 #ifndef _LISTA_
 #define _LISTA_
 
 typedef struct tLista Lista;
 
+/**
+ * @brief Inicializa dinamicamente uma @ref Lista
+ * 
+ * @return Lista* Uma nova instancia de @ref Lista
+ */
 Lista *InicializaLista();
 
-void *LiberaLista(Lista *lista);
+/**
+ * @brief Libera a @p lista e todos os seus elementos da memória em que foram dinamicamente alocados
+ * 
+ * @param lista Uma instancia de @ref Lista
+ * @param liberaElem Uma funcao destrutora dos elementos da lista
+ */
+void LiberaLista(Lista *lista, void (*liberaElem)(void *));
 
+/**
+ * @brief Adiciona um elemento @p elem ao final da lista @p lista
+ * 
+ * @param lista Uma instancia de @ref Lista de mesmo tipo que @p elem
+ * @param elem O elemento que sera adicionado
+ */
 void AdicionaElementoLista(Lista *lista, void *elem);
 
 /**
--- src/libs/lista.c
+++ src/libs/lista.c
@@ -2,6 +2,8 @@
 
 #include "lista.h"
 
+static const int ritmo_crescimento_lista = 5;
+
 struct tLista {
     int capacidade;
     int qtd;
@@ -9,18 +11,43 @@ struct tLista {
 };
 
 Lista *InicializaLista() {
+    Lista *lista = malloc(sizeof *lista);
 
+    const int capacidade_inicial = 5;
+
+    lista->capacidade = capacidade_inicial;
+    lista->qtd = 0;
+    lista->arr = malloc(lista->capacidade * __SIZEOF_POINTER__);
+
+    return lista;
 }
 
-void *LiberaLista(Lista *lista) {
+void LiberaLista(Lista *lista, void (*liberaElem)(void *)) {
+    for (int i = 0; i < lista->qtd; i++)
+        (*liberaElem)(lista->arr[i]);
 
+    free(lista->arr);
+
+    free(lista);
 }
 
 void AdicionaElementoLista(Lista *lista, void *elem) {
+    if (lista->qtd == lista->capacidade) {
+        lista->capacidade += ritmo_crescimento_lista;
+        lista->arr =
+            realloc(lista->arr, lista->capacidade * __SIZEOF_POINTER__);
+    }
 
+    lista->arr[lista->qtd++] = elem;
 }
 
 void *PopLista(Lista *lista) {
+    if (lista->qtd < (lista->capacidade - 2 * ritmo_crescimento_lista)) {
+        lista->capacidade -= ritmo_crescimento_lista;
+        lista->arr =
+            realloc(lista->arr, lista->capacidade * __SIZEOF_POINTER__);
+    }
+
     void *r = lista->arr[lista->qtd - 1];
     lista->arr[lista->qtd - 1] = NULL;
     lista->qtd--;
